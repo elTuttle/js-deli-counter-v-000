7268ad47e32dd36b7674ae5cd802dbe73dc93b9a
John Tuttle <thejwt@gmail.com>
2017-09-13 06:07:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ var katzDeli = []
 function takeANumber(katzDeliLine, name){
   var deliPlace = katzDeli.length + 1
   katzDeli.push(`Welcome, ${name}. You are number ${deliPlace} in line` )
+  return katzDeli
 }
