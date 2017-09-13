1fbf83c7ef87a3cebaf20b6e0d5b497b4ecf0442
John Tuttle <thejwt@gmail.com>
2017-09-13 06:52:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,14 @@ function nowServing(deliLine){
       return "There is nobody waiting to be served!"
   }
 }
+
+function currentLine(line){
+  var response = "The line is currently"
+  var deliLineLength = line.length + 1
+  if(line.length > 0 ){
+    response + ": "
+    for(var i=0;i<line.length;i++){
+      response + ` ${deliLineLength}. ${line[i]} `
+    }
+  }
+}
